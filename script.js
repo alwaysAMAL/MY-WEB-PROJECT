2832bd5e27418802f059e461d61f91c602bee422
AMAL <amalkochu000@gmail.com>
2024-01-11 12:18:35 +0530

    commit

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const toggle = document.getElementById("toggle-btn");
 toggle.addEventListener("change", function () {
   document.body.style.backgroundColor = this.checked ? "rgb(10, 10, 42)" : "";
-  document.querySelector(".intro").style.color = "#fff";
-  document.querySelector(".salute").style.color = "#fff";
+  if (this.checked) {
+    document.querySelector(".intro").style.color = "#fff";
+    document.querySelector(".salute").style.color = "#fff";
+  } else {
+    document.querySelector(".intro").style.color = "#000";
+    document.querySelector(".salute").style.color = "#000";
+  }
 });
